7173d7df61ccc9f18e561249016af1ac8e15cdb4
Hph1998 <792396951@qq.com>
2021-01-26 23:32:32 +0800

    替换空格

---
+++ src/ReplaceSpace03.java
@@ -0,0 +1,30 @@
+/**
+ * @author Lucas
+ * @version 1.0.0
+ * @ClassName ReplaceSpace03.java
+ * @Description 替换空格
+ * @createTime 2021年01月26日 23:17:00
+ */
+public class ReplaceSpace03 {
+
+    public static String replaceSpace(String s) {
+        //单线程就不用考虑线程安全 当然选择性能更高的StringBuilder
+        StringBuilder res = new StringBuilder();
+        StringBuilder stringBuffer = new StringBuilder(s);
+        for (int i = 0; i < stringBuffer.length(); i++){
+            //截取当前位置的字符
+            String temp = stringBuffer.substring(i,i+1);
+            if (" ".equals(temp)){ //与空格相等则追加%20
+                res.append("%20");
+            }else { //否则直接追加当前字符
+                res.append(temp);
+            }
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(replaceSpace("We are happy."));
+    }
+
+}
