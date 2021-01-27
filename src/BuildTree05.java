3acb7ab02b018b7e8f95fafd1a35ea4ed737a5c0
Hph1998 <792396951@qq.com>
2021-01-27 18:26:15 +0800

    重建二叉树

---
+++ src/BuildTree05.java
@@ -0,0 +1,19 @@
+/**
+ * @author Lucas
+ * @version 1.0.0
+ * @ClassName BuildTree05.java
+ * @Description 重建二叉树
+ * @createTime 2021年01月27日 15:21:00
+ */
+public class BuildTree05 {
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
---
+++ src/TreeNode.java
@@ -0,0 +1,17 @@
+/**
+ * @author Lucas
+ * @version 1.0.0
+ * @ClassName TreeNode.java
+ * @Description 树结构
+ * @createTime 2021年01月27日 15:21:00
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val = x;
+    }
+
+}
