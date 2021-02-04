b8036b9691f76f86fc15767f57e973458252c65e
Hph1998 <792396951@qq.com>
2021-02-04 10:50:22 +0800

    打印从1到最大的n位数

---
+++ src/PrintNumbers16.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/**
+ * @author Lucas
+ * @version 1.0.0
+ * @ClassName PrintNumbers16.java
+ * @Description 打印从1到最大的n位数
+ * @createTime 2021年02月04日 10:30:00
+ */
+public class PrintNumbers16 {
+
+    public static int[] printNumbers(int n) {
+        // 规律 计算出数组的长度
+        int count = (int) Math.pow(10,n) - 1;
+        int[] res = new int[count];
+        // 只能是循环插入数据了
+        for (int i = 0; i < count; i++){
+            res[i] = i + 1;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(printNumbers(3)));
+    }
+}
