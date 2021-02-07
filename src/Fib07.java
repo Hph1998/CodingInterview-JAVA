f01c254b2570e6485d822ac9b002475ae4b2b47b
Hph1998 <792396951@qq.com>
2021-02-07 23:04:54 +0800

    斐波那契数列

--- src/Fib07.java
+++ src/Fib07.java
@@ -1,13 +1,17 @@
 /**
+ * 斐波那契数列
  * @author Lucas
- * @version 1.0.0
- * @ClassName Fib07.java
- * @Description 斐波那契数列
- * @createTime 2021年01月30日 14:06:00
+ * @date 2021/01/30 14:06:00
  */
 public class Fib07 {
 
-    //无脑递归 超出时间限制 哈哈哈哈哈哈
+    /**
+     * 无脑递归 超出时间限制
+     * @author Lucas
+     * @date 2021/02/07 22:48
+     * @param n 斐波拉契第几个
+     * @return int
+     */
     /*public static int fib(int n) {
         if(n == 0){
             return 0;
