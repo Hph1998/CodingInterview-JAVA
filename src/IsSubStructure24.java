129e22aa30671948d878607332b7dc62d3d3b870
Hph1998 <792396951@qq.com>
2021-02-07 20:59:58 +0800

    树的子结构

---
+++ src/IsSubStructure24.java
@@ -0,0 +1,18 @@
+/**
+ * @author Lucas
+ * @version 1.0.0
+ * @ClassName IsSubStructure24.java
+ * @Description 树的子结构
+ * @createTime 2021年02月07日 17:59:00
+ */
+public class IsSubStructure24 {
+
+    public static boolean isSubStructure(TreeNode A, TreeNode B) {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
