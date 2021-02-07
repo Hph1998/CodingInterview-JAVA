32ff78a22b20f3786eb111c96a4472c390c00fc7
Hph1998 <792396951@qq.com>
2021-02-07 23:07:57 +0800

    青蛙跳台阶问题

--- src/NumWays08.java
+++ src/NumWays08.java
@@ -1,16 +1,20 @@
 /**
+ * 青蛙跳台阶问题
  * @author Lucas
- * @version 1.0.0
- * @ClassName NumWays08.java
- * @Description 青蛙跳台阶问题
- * @createTime 2021年01月30日 14:42:00
+ * @date 2021/01/30 14:42
  */
 public class NumWays08 {
 
-    /**其实就是斐波拉契数列的思路
+    /**
+     * 其实就是斐波拉契数列的思路
      * 走一台阶一种方式
      * 走两台阶两种方式
-     * 走三台阶即可拆分为走一台阶+走两台阶*/
+     * 走三台阶即可拆分为走一台阶+走两台阶
+     * @author Lucas
+     * @date 2021/02/07 22:38
+     * @param n 台阶数
+     * @return int
+     */
     public static int numWays(int n) {
         int a = 1;
         int b = 1;
